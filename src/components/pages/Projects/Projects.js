790a6ae9a89a8329a2207387ac923fe70ab7767d
garandrew102 <garandrew102@gmail.com>
2020-12-16 21:28:10 -0500

    added cards to projects page

--- src/components/pages/Projects/Projects.js
+++ src/components/pages/Projects/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
-import Cards from "../Cards/Cards";
-import "../../App.css";
+import Cards from "../../Cards/Cards";
+import "../../pages/Projects/Projects.css";
+import "../../../App.css";
 
 const Projects = () => {
   return (
