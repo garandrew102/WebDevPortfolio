8a4dfef41c0963c027ca14fb13f0f580059015b4
garandrew102 <garandrew102@gmail.com>
2020-12-23 22:50:30 -0500

    added video option for aboutme page

--- src/components/pages/About Me/AboutMe.js
+++ src/components/pages/About Me/AboutMe.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./AboutMe.css";
+import contactbg from "../../../videos/contactbg.mp4";
 
 function AboutMe() {
   return (
     <div className="about-me">
+      <video autoPlay loop muted>
+        <source src={contactbg} type="video/mp4" />
+      </video>
       <h1>About Me</h1>
     </div>
   );
