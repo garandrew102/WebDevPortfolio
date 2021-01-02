bcaf35ae8ae4aa553cc9f453c1e6fc90dbc17550
garandrew102 <garandrew102@gmail.com>
2021-01-02 14:19:03 -0500

    styled form

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -41,44 +41,50 @@ class Contact extends Component {
         <video autoPlay loop muted>
           <source src={contactbg} type="video/mp4" />
         </video>
-        <h1>Let's Connect</h1>
-        <hr />
+
         <div className="form">
+          <h1>Let's Connect</h1>
           <Form onSubmit={this.handleSubmit}>
-            <Form.Group>
-              <Form.Label htmlFor="full-name">Name:</Form.Label>
-              <Form.Control
-                id="full-name"
-                name="name"
-                type="text"
-                value={this.state.name}
-                onChange={this.handleChange}
-              ></Form.Control>
-            </Form.Group>
-            <hr />
-            <Form.Group>
-              <Form.Label htmlFor="email">Email:</Form.Label>
-              <Form.Control
-                id="email"
-                name="email"
-                type="email"
-                value={this.state.email}
-                onChange={this.handleChange}
-              ></Form.Control>
-            </Form.Group>
-            <hr />
-            <Form.Group>
-              <Form.Label htmlFor="message">Message:</Form.Label>
-              <Form.Control
-                id="message"
-                name="message"
-                as="textarea"
-                rows="5"
-                value={this.state.message}
-                onChange={this.handleChange}
-              ></Form.Control>
-            </Form.Group>
-            <hr />
+            <div className="name">
+              <Form.Group>
+                <Form.Label htmlFor="full-name">Name:</Form.Label>
+                <Form.Control
+                  id="full-name"
+                  name="name"
+                  type="text"
+                  placeholder="Enter Name Here"
+                  value={this.state.name}
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+            </div>
+            <div className="email">
+              <Form.Group>
+                <Form.Label htmlFor="email">Email:</Form.Label>
+                <Form.Control
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="Enter Email Here"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+            </div>
+            <div className="message">
+              <Form.Group>
+                <Form.Label htmlFor="message">Message:</Form.Label>
+                <Form.Control
+                  id="message"
+                  name="message"
+                  as="textarea"
+                  rows="5"
+                  placeholder="Enter Message Here"
+                  value={this.state.message}
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+            </div>
             <div className="button">
               <Button
                 buttonStyle="btn--outline"
